5f0d1e354de62ae263ce2b736a63a3ca6e9668df
omkarkokil <nsjustice21@gmail.com>
2024-07-20 18:57:49 +0530

    footer updated

--- components/custom/Footer.tsx
+++ components/custom/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="w-full">
       <Separator />
       <p className="text-center sm:text-sm text-xs text-gray-700 p-5">
-        © Copyright 2024 - Developed by Omkar kokil. All right reserved.
+        Developed by Omkar kokil.
       </p>
     </div>
   );
